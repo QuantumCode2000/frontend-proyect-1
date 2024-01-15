4af40b774355030edfba39518c327090160e758e
Limbert Eddy Loza Gutierrez <lozagutierrezlimbert@gmail.com>
2024-01-15 09:35:17 -0400

    Mejoras del estilo del perfil

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,59 +1,39 @@
 import "./Perfil.styles.css"
 const Perfil = () => {
     return (
-        // <main>
-        //     <div className="container">
-        //         <div className="main">
-        //             <div className="topbar">
-        //                 <h1>Pefil</h1>
-        //             </div>
-        //             <div className="row">
-        //                 <div className="youself">
-        //                     <div className="card text-center sidebar">
-        //                         <h2 className="m-3 pt-3">Perfil</h2>
-        //                         <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className="rounded-circle" width={150}></img>
-        //                             <div className="mt-3">
-        //                             <h3>Nombre del usuario</h3>
-        //                             <a href="">Correo</a>
-        //                             <a href="">Tipo de Usuario</a>
-        //                             </div>
-        //                     </div>
-        //                 </div>
-        //                 <div className="conten">
-        //                     <div className="card mb-3 content">
-        //                         <div className="card-body">
-        //                             <div className="row">
-        //                                 <div className="col-md 3">
-        //                                     <h5>Nombre Completo</h5>
-        //                                 </div>
-        //                                 <div className="col-md-9 text-secundary">
-        //                                     Jorge Tancara
-        //                                 </div>
-        //                             </div>
-        //                             <div className="row">
-        //                                 <h5>Correo</h5>
-        //                             </div>
-        //                             <p>jorgetancar@gmail.com</p>
-        //                         </div>  
-        //                     </div>
-        //                 </div>
-        //             </div>    
-        //         </div>
-        //     </div>
-        // </main>
         <body>
         <main className="container">
              <div className="box1">
-                <p>Perfil</p>
+                <h1 className="header-title">Perfil</h1>
              </div>
              <div className="box2">
-                <p>Panel</p>
+                <h2 className="body-title">Panel</h2>
              </div>
              <div className="box3">
-                <p>Datos</p>
+                <h2>Datos</h2>
+                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className="rounded-circle" width={150}></img>
+                
+                
+                <h3></h3><button>Cargar Imagen</button>
+                <h3>Nombre del usuario</h3>
+                <p>Correo</p>
+                <p>Tipo de Usuario</p>
              </div>
              <div className="box4">
-                <p>Informacion</p>
+                <h2>Información</h2>
+                <th>Nombre/s:</th>
+                <th>Jorge Luis</th> 
+                <th>Apellido Paterno:</th>
+                <th>Tancara</th> 
+                <th>Apellido Materno:</th>
+                <th>Tancara</th> 
+                <th>Correo:</th>   
+                <th>jorgetancara@gmail.com</th>
+                <th>Contraseña:</th>   
+                <th>xxxxxxxxxx</th>
+                <th>Tipo de Usuario:</th>   
+                <th>Administrador</th>
+                <h1></h1><button>Actualizar Información</button>
              </div>
         </main>
         </body>
