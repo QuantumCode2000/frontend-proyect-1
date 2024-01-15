91e2701e52629a271788fd3fbb09a77532791e3f
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2024-01-15 14:32:23 -0400

    agregando grid a stylos de Perfil

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,48 +1,46 @@
 import "./Perfil.styles.css"
 const Perfil = () => {
-    return (
+   return (
 
-        <div className="container-perfil">
+      <div className="container-perfil">
          <main className="container">
+            <div className="datos-box">
+               <h2>Datos</h2>
+               <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className="rounded-circle" width={ 150 }></img>
+               <h3></h3><button>Cargar Imagen</button>
+               <h3>Nombre del usuario</h3>
+               <p>Correo</p>
+               <p>Tipo de Usuario</p>
+            </div>
+            <div className="informacion-box">
+               <h2>Información</h2>
 
-             <div className="box1">
-                <h1 className="header-title">Perfil</h1>
-             </div>
-             <div className="box2">
-                <h2 className="body-title">Panel</h2>
-             </div>
-             <div className="box3">
-                <h2>Datos</h2>
-                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className="rounded-circle" width={150}></img>
-                
+               <th>Nombre/s:</th>
+
+               <th>Jorge Luis</th>
+               <th>Apellido Paterno:</th>
+
+               
+               <th>Apellido Materno:</th>
+
+               <th>Tancara</th>
+               <th>Correo:</th>
+
+               <th>jorgetancara@gmail.com</th>
+
+               <th>Contraseña:</th>
+
+               <th>xxxxxxxxxx</th>
+               <th>Tipo de Usuario:</th>
+
+               <th>Administrador</th>
+               <button>Actualizar Información</button>
+            </div>
+         </main>
+      </div>
 
 
-                <h3></h3><button>Cargar Imagen</button>
-                <h3>Nombre del usuario</h3>
-                <p>Correo</p>
-                <p>Tipo de Usuario</p>
-             </div>
-             <div className="box4">
-                <h2>Información</h2>
-                <th>Nombre/s:</th>
-                <th>Jorge Luis</th> 
-                <th>Apellido Paterno:</th>
-                <th>Tancara</th> 
-                <th>Apellido Materno:</th>
-                <th>Tancara</th> 
-                <th>Correo:</th>   
-                <th>jorgetancara@gmail.com</th>
-                <th>Contraseña:</th>   
-                <th>xxxxxxxxxx</th>
-                <th>Tipo de Usuario:</th>   
-                <th>Administrador</th>
-                <h1></h1><button>Actualizar Información</button>
-             </div>
-        </main>
-        </div>
-        
-        
-    )
+   )
 }
 
 export default Perfil
