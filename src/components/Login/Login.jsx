9efb95735c48a4fcd61926bdaab530b986b238c0
Limbert Eddy Loza Gutierrez <lozagutierrezlimbert@gmail.com>
2024-01-15 14:27:44 -0400

    maquetado del login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,23 @@
+import "./Login.styles.css"
 const Login = () => {
     return (
-        <div>Login Dev</div>
+        <div className="container-login">
+        <main className="container-log">
+        <div className="box1-login">
+                <h1 className="header-login">Inicio</h1>
+             </div>
+             <div className="box2-login">
+                <h2>Inicio de Sesión</h2>
+                <img src="https://cdn-icons-png.flaticon.com/512/2919/2919600.png" className="rounded-circle" width={150}></img>
+                <form action="">Ingreso de Datos</form>
+                <h3>Usuario</h3>
+                <input type="text"  />
+                <h3>Contraseña</h3>
+                <input type="text"  />
+                <button className="button-login">Iniciar Sesión</button>
+             </div>
+        </main>
+        </div>
     )
 }
 
